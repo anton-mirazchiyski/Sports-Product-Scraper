fbd55c521cd7b0e8090ebb3f3b22f300c02f0154
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-02-06 20:02:31 +0200

    Attempt to extract data

--- task/task/spiders/products_spider.py
+++ task/task/spiders/products_spider.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 import scrapy
+from scrapy.http import Response
 
 
 class SportsProductSpider(scrapy.Spider):
@@ -7,6 +10,17 @@ class SportsProductSpider(scrapy.Spider):
     allowed_domains = ['academy.com']
 
     start_urls = [
-        'https://www.academy.com/'
+        'https://www.academy.com/p/nike-womens-court-legacy-next-nature-shoes'
     ]
-    
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        name = response.css('h1::text').get().strip()
+        price = response.css('span.pricing.nowPrice::text').get()
+
+        product_data = {
+            'name': name,
+            'price': price,
+        }
+
+        yield product_data
+
