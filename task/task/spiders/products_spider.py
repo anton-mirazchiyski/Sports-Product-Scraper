645bd7f923f03a428d47bde38cb48f7333d71281
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-02-08 20:55:15 +0200

    Organize code better

--- task/task/spiders/products_spider.py
+++ task/task/spiders/products_spider.py
@@ -16,14 +16,15 @@ class SportsProductSpider(scrapy.Spider):
     ]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        name = response.css('h1::text').get().strip()
-        price = response.css('span.pricing.nowPrice::text').get()
-
         driver = self.setup_driver(urls_idx=0)
-        current_color = driver.find_element(by=By.CSS_SELECTOR, value='span.swatchName--KWu4Q').text
+
+        name = self.get_single_data_by_selector('h1', response, driver)
+        price = self.get_single_data_by_selector('span.pricing.nowPrice', response, driver)
+        current_color = self.get_single_data_by_selector('span.swatchName--KWu4Q', response, driver)
+
         self.close_driver(driver)
 
-        print(current_color)
+        print(name, price, current_color)
 
         product_data = {
             'name': name,
@@ -32,6 +33,19 @@ class SportsProductSpider(scrapy.Spider):
         }
 
         yield product_data
+
+    @staticmethod
+    def get_single_data_by_selector(selector, response, driver):
+        """
+        Tries to get/fetch element by normal CSS Selector using only Scrapy,
+        if not - it is dynamic and gets it with Selenium
+        """
+
+        data = response.css(selector + '::text').get()
+        if data is None:
+            data = driver.find_element(by=By.CSS_SELECTOR, value=selector).text
+
+        return data.strip()
 
     def setup_driver(self, urls_idx):
         driver = webdriver.Firefox()
