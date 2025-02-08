9b0560c520e79233dde46493b9f6a058f39db4db
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-02-08 20:26:06 +0200

    Extract current product color using Selenium

--- task/task/spiders/products_spider.py
+++ task/task/spiders/products_spider.py
@@ -2,6 +2,8 @@ from typing import Any
 
 import scrapy
 from scrapy.http import Response
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class SportsProductSpider(scrapy.Spider):
@@ -17,10 +19,24 @@ class SportsProductSpider(scrapy.Spider):
         name = response.css('h1::text').get().strip()
         price = response.css('span.pricing.nowPrice::text').get()
 
+        driver = self.setup_driver(urls_idx=0)
+        current_color = driver.find_element(by=By.CSS_SELECTOR, value='span.swatchName--KWu4Q').text
+        self.close_driver(driver)
+
+        print(current_color)
+
         product_data = {
             'name': name,
             'price': price,
+            'color': current_color,
         }
 
         yield product_data
 
+    def setup_driver(self, urls_idx):
+        driver = webdriver.Firefox()
+        driver.get(self.start_urls[urls_idx])
+        return driver
+
+    def close_driver(self, driver):
+        driver.quit()
