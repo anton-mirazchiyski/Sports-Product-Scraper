2951f1eef11f885b20f32e5af033c359fb759439
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-02-14 16:19:44 +0200

    Update methods

--- task/task/spiders/products_spider.py
+++ task/task/spiders/products_spider.py
@@ -29,15 +29,14 @@ class SportsProductSpider(scrapy.Spider):
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         # driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        # time.sleep(5)
 
-        name = self.get_single_data_by_selector('h1.productTitle--FWmyK', response, self.driver)
-        price = self.get_single_data_by_selector('span.pricing.nowPrice', response, self.driver)
-        collected_data = self.get_data_by_selector('span.swatchName--KWu4Q', response, self.driver)
+        name = self.get_single_data_by_selector('h1.productTitle--FWmyK', response)
+        price = self.get_single_data_by_selector('span.pricing.nowPrice', response)
+        collected_data = self.get_data_by_selector('span.swatchName--KWu4Q', response)
 
         current_colour, current_size, _ = collected_data if collected_data else '', '', ''
 
-        available_colours = self.get_all_available_colors(response, self.driver)
+        available_colours = self.get_all_available_colors(response)
 
         self.driver.quit()
 
@@ -51,20 +50,19 @@ class SportsProductSpider(scrapy.Spider):
 
         yield product_data
 
-    def get_all_available_colors(self, response, driver):
-        button_elements = driver.find_elements(by=By.CSS_SELECTOR, value='button.buttonWrapper--S9sgu')
+    def get_all_available_colors(self, response):
+        button_elements = self.driver.find_elements(by=By.CSS_SELECTOR, value='button.buttonWrapper--S9sgu')
         available_colours = []
 
         for button_element in button_elements:
-            colour = self.get_single_data_by_selector('span.swatchName--KWu4Q', response, driver)
+            colour = self.get_single_data_by_selector('span.swatchName--KWu4Q', response)
             available_colours.append(colour)
             button_element.click()
             time.sleep(random.uniform(2, 5))
 
         return available_colours
 
-    @staticmethod
-    def get_single_data_by_selector(selector, response, driver):
+    def get_single_data_by_selector(self, selector, response):
         """
         Tries to get/fetch element by normal CSS Selector using only Scrapy,
         if not - it is dynamic and gets it with Selenium
@@ -73,21 +71,19 @@ class SportsProductSpider(scrapy.Spider):
         data = response.css(selector + '::text').get()
         if data is None:
             try:
-                data = driver.find_element(by=By.CSS_SELECTOR, value=selector).text
+                data = self.driver.find_element(by=By.CSS_SELECTOR, value=selector).text
             except NoSuchElementException:
                 data = None
 
         return data.strip() if data else None
 
-    @staticmethod
-    def get_data_by_selector(selector, response, driver):
+    def get_data_by_selector(self, selector, response):
         """
         Same as the method 'get_single_data_by_selector' but returns a collection of data
         """
 
         data = response.css(selector + '::text').getall()
-        if data is None:
-            data = [element.text for element in driver.find_elements(by=By.CSS_SELECTOR, value=selector)]
+        if not data:
+            data = [element.text for element in self.driver.find_elements(by=By.CSS_SELECTOR, value=selector)]
 
-        # data = [text.strip() for text in data]
-        return data
+        return [text.strip() for text in data] if data else []
